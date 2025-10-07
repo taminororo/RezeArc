44d0280d42cfb6563c6020a9521bca25461c86a8
taminororo <169162271+taminororo@users.noreply.github.com>
2025-10-08 01:27:08 +0900

    [add]mapDetailコンポーネントの実装

---
+++ src/app/detailMap/page.tsx
@@ -0,0 +1,9 @@
+import MapDetail from "@/components/mapDetail";
+
+export default function Page() {
+  return (
+    <main className="min-h-dvh w-full flex justify-center items-start p-4">
+      <MapDetail />
+    </main>
+  );
+}
