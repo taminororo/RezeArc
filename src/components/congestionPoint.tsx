565b85231c80a12b3813ffe2145cba0b9239bc23
taminororo <169162271+taminororo@users.noreply.github.com>
2025-10-08 16:23:52 +0900

    [add]これからdetailMapも変更する

--- src/components/congestionPoint.tsx
+++ src/components/congestionPoint.tsx
@@ -1,3 +1,4 @@
+//src/components/congestionPoint.tsx
 "use client";
 
 import React from "react";
--- src/components/mapDetail.tsx
+++ src/components/mapDetail.tsx
@@ -1,3 +1,4 @@
+// src/components/mapDetail.tsx
 "use client";
 
 import React, { useEffect, useMemo, useState } from "react";
